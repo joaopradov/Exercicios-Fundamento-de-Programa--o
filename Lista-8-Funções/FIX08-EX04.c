45408daf72d77d21a4f633332bc372e29f2866a8
João Vitor Prado <joaopradovitor99@gmail.com>
2025-02-04 10:51:16 -0300

    Exercicio 04 - Lista 08

--- Lista-8-Funções/FIX08-EX04.c
+++ Lista-8-Funções/FIX08-EX04.c
@@ -18,3 +18,88 @@ Entre com a opção:
 
 * Utilize uma função para apresentar o menu inicial e uma função de cada tipo para resolver as operações. Cada operações (adição; subtração; multiplicação; divisão) deve usar um tipo de função (sem retorno e sem parâmetro; sem retorno e com parâmetro; com retorno e sem parâmetro; com retorno e com parâmetro;) - não vale repetir o uso.
 */
+
+#include <stdio.h>
+
+void mostrarMenu(){
+    printf("**********");
+    printf("Calculadora");
+    printf("1. Adição");
+    printf("2. Subtração");
+    printf("3. Multiplicação");
+    printf("4. Divisão");
+    printf("5. Sair");
+    printf("**********");
+}
+
+void adicao(int n1, int n2){
+    printf("O resultado da adicao é %d", n1 + n2);
+}
+
+void subtracao() {
+    int n1, n2;
+    printf("Digite o primeiro numero: ");
+    scanf("%d", &n1);
+    printf("Digite o segundo numero: ");
+    scanf("%d", &n2);
+    printf("O resultado da subtracao e %d", n1 - n2);
+}
+
+int multiplicacao() {
+    int n1, n2;
+    printf("Digite o primeiro numero: ");
+    scanf("%d", &n1);
+    printf("Digite o segundo numero: ");
+    scanf("%d", &n2);
+    return n1*n2;
+}
+
+float divisao(float n1, float n2) {
+    return n1 / n2;
+}
+
+int main(){
+    int opcao;
+    int n1, n2;
+    float resultado;
+    float num1, num2;
+
+    do{
+        mostrarMenu();
+        scanf("%d", &opcao);
+
+        switch (opcao)
+        {
+        case 1:
+            printf("Digite o primeiro numero: ");
+            scanf("%d", &n1);
+            printf("Digite o segundo numero: ");
+            scanf("%d", &n2);
+            adicao(n1,n2);
+            break;
+        case 2:
+            subtracao();
+            break;
+        case 3:
+            resultado = multiplicacao();
+            printf("O resultado da multiplicação e %.2f", resultado);
+            break;
+        case 4:
+            printf("Digite o primeiro numero: ");
+            scanf("%f", &num1);
+            printf("Digite o segundo numero: ");
+            scanf("%f", &num2);
+            resultado = divisao(num1, num2);
+            printf("O resultado da divisao e %.2f", resultado);
+            break;
+        case 5:
+            printf("Saindo do programa...\n");
+            break;
+        default:
+            printf("ERRO! Opcao invalida tente novamente! \n");
+            break;
+        }
+    }while (opcao != 5);
+
+    return 0;
+}
