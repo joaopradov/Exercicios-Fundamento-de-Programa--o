a6e7efa49fdc71e7ef4eef7be1db92230ff0b08b
João Vitor Prado <joaopradovitor99@gmail.com>
2025-02-04 10:26:34 -0300

    Exercicio 02 - Lista 08

--- Lista-8-Funções/FIX08-EX02.c
+++ Lista-8-Funções/FIX08-EX02.c
@@ -6,3 +6,24 @@ Data: 03/02/2025
 
 FIX08_EX02: Faça um programa em Linguagem C que escreva uma função para calcular a média aritmética de 3 valores. A função deve receber os valores como parâmetro, calcular e apresentar o resultado na tela.
 */
+
+#include <stdio.h>
+
+void calcMedia(float nt1, float nt2, float nt3){
+    float media = (nt1 + nt2 + nt3) / 3;
+    printf("A media foi de %.2f", media);
+}
+
+int main(){
+    float nt1, nt2, nt3;
+    printf("Digite a primeira nota: ");
+    scanf("%f", &nt1);
+    printf("Digite a segunda nota: ");
+    scanf("%f", &nt2);
+    printf("Digite a tereceira nota: ");
+    scanf("%f", &nt3);
+
+    calcMedia(nt1, nt2,nt3);
+
+    return 0;
+}
