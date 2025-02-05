1589b35344c1e91631adc2740fcd928b7c40765e
João Vitor Prado <joaopradovitor99@gmail.com>
2025-02-05 14:18:22 -0300

    Exercicio 5 - Lista 9

--- Lista-9-Estrutura de dados heterogênea/FIX09-EX05.c
+++ Lista-9-Estrutura de dados heterogênea/FIX09-EX05.c
@@ -6,3 +6,47 @@ Data: 04/02/2025
 
 FIX09_EX05: Construa uma função projetada para receber datas do usuário. Esta função deve continuar solicitando uma data até que o usuário forneça uma data válida. Quando uma data válida for fornecida, a função deve retornar essa data.
 */
+
+#include <stdio.h>
+#include <stdbool.h>
+
+struct Data
+{
+    int dia;
+    int mes;
+    int ano;
+};
+
+typedef struct Data Data;
+
+int validacao (Data Data){
+    if (Data.dia < 1 || Data.dia > 30)
+    {
+        return 0;
+    }
+    if (Data.mes < 1 || Data.mes > 12)
+    {
+        return 0;
+    }
+    if (Data.ano < 1900 || Data.ano > 2024)
+    {
+        return 0;
+    }
+    return 1;
+}
+
+int main(){
+    Data Data;
+    do{
+        printf("Digite o dia: ");
+    scanf("%d", &Data.dia);
+    printf("Digite o mes: ");
+    scanf("%d", &Data.mes);
+    printf("Digite o ano: ");
+    scanf("%d", &Data.ano);
+    }while (validacao(Data) == 0);
+
+    printf("Data: %.2d/%.2d/%.4d", Data.dia, Data.mes, Data.ano);
+    
+    return 0;
+}
