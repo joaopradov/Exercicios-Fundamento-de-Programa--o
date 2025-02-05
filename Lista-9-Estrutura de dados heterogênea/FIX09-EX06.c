f5f5a6a86dffbb858269168e46eba31fa8a2f793
João Vitor Prado <joaopradovitor99@gmail.com>
2025-02-05 14:33:21 -0300

    ex6

--- Lista-9-Estrutura de dados heterogênea/FIX09-EX06.c
+++ Lista-9-Estrutura de dados heterogênea/FIX09-EX06.c
@@ -6,3 +6,16 @@ Data: 04/02/2025
 
 FIX09_EX06: Construa uma função que recebe 2 (duas) datas como parâmetro (struct) e retorne um inteiro representando a diferença (em dias) entre as duas datas. Faça um programa em Linguagem C que demostre o uso desta função.
 */
+
+#include <stdio.h>
+#include <stdbool.h>
+
+struct Data
+{
+    int dia;
+    int mes;
+    int ano;
+};
+
+typedef struct Data Data;
+
