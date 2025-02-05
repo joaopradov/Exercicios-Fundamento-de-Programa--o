880993932df0df0f7cd97b322c73640ad06c96db
João Vitor Prado <joaopradovitor99@gmail.com>
2025-02-05 10:54:36 -0300

    Exercicio 1 - List 9

--- Lista-9-Estrutura de dados heterogênea/FIX09-EX01.c
+++ Lista-9-Estrutura de dados heterogênea/FIX09-EX01.c
@@ -8,3 +8,28 @@ FIX09_EX01: Faça um programa em Linguagem C que receba e apresente na tela uma 
 */
 
 #include <stdio.h>
+
+// Definido a estrutura dos registros que serão fornecidos/ usado.
+struct Data
+{
+    int dia;
+    int mes;
+    int ano;
+};
+// Aqui foi ajustado com o typedef para que pudessse apenas digitar 'Data' e não o 'struct Data'.
+typedef struct Data Data;
+
+int main(){
+    // Deifinindo a variavel 'Data', para que não de erro no codigo
+    Data Data;
+    printf("Digite o dia: ");
+    scanf("%d", &Data.dia);
+    printf("Digite o mes: ");
+    scanf("%d", &Data.mes);
+    printf("Digite o ano: ");
+    scanf("%d", &Data.ano);
+
+    printf("A data digitada foi %.2d/%.2d/%.4d",Data.dia, Data.mes, Data.ano);
+
+    return 0;
+}
