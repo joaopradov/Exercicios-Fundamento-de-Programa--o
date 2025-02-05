770f5c82713006ec8420fd47f374d56b526e247b
João Vitor Prado <joaopradovitor99@gmail.com>
2025-02-05 14:09:57 -0300

    Exercicio 4 - Lista 9

--- Lista-9-Estrutura de dados heterogênea/FIX09-EX04.c
+++ Lista-9-Estrutura de dados heterogênea/FIX09-EX04.c
@@ -8,3 +8,50 @@ FIX09_EX04: Construa uma função que receba uma data por parâmetro (struct), v
 
 Regra de validação: dias entre 1 e 30 (inclusive); mês entre 1 e 12 (inclusive); anos entre 1900 e 2024 (inclusive);
 */
+
+#include <stdio.h>
+#include <stdbool.h>
+
+struct Data
+{
+    int dia;
+    int mes;
+    int ano;
+};
+
+typedef struct Data Data;
+
+// Na função ele vai retornar 0 se estiver inválido e 1 caso esteja válida a data.
+int validacao (Data Data)
+{
+    if (Data.dia < 1 || Data.dia > 30)
+    {
+        return 0;
+    }
+    if (Data.mes < 1 || Data.mes > 12)
+    {
+        return 0;
+    }
+    if (Data.ano < 1900 || Data.ano > 2024)
+    {
+        return 0;
+    }
+    return 1;
+}
+
+int main() {
+    Data Data;
+    printf("Digite o dia: ");
+    scanf("%d", &Data.dia);
+    printf("Digite o mes: ");
+    scanf("%d", &Data.mes);
+    printf("Digite o ano: ");
+    scanf("%d", &Data.ano);
+
+    int resultado;
+    resultado = validacao(Data);
+
+    printf("%d", resultado);
+
+    return 0;
+}
